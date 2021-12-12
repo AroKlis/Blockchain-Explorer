0a4d145386168b26278afdadc16bc00851df6113
Karolis Kubilis <krlskblus@gmail.com>
2021-12-12 10:21:31 +0000

    Basic website template with basic css

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,62 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header>
+      <img src="https://xe.network/assets/logo.svg"/>
+    </header>
+    <body>
+      <div class="search-box">
+        <p>Search the Xi Blockchain</p>
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
+          v-model="query"
+        />
+      </div>
+      <div class = "search-result">
+        
+      </div>
+      <div class = "block-count">
+        <p> Current Block Count</p>
+        <p class = "number of blocks">{{ fetch(`${this.url_base}"/blocks"`)}}</p>
+      </div>
+      <table class="blockchain-tabel">
+        <tr>
+          <th class = "tabel-header">Block Number</th>
+          <th class = "tabel-header">Address</th>
+          <th class = "tabel-header">Hash</th>
+          <th class = "tabel-header">Timestamp</th>
+        </tr>
+                            
+      </table>
+    </body>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'app',
+  data () {
+    return {
+      url_base: 'https://xi.test.network/',
+      query: '',
+      blockNumber: {},
+      Address: {},
+      Hash: {},
+      Timestamp: {},
+    }
+  },
+  methods: {
+    fetchWeather (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}"/blocks"`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    
+  }
 }
 </script>
 
@@ -20,4 +69,65 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+html{
+  padding: 0;
+  margin: 0;
+  border: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+
+}
+
+header{
+  margin:0;
+  padding: 0;
+  background-color: rgba(0, 0, 0, 0.9);
+}
+
+body {
+  background-color: rgba(255, 255, 255, 0.1);
+  margin:0px;
+  padding: 0px;
+  font-family: system-ui,-apple-system,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji;
+}
+
+.search-box{
+  background-color: rgba(0, 0, 0, 0.80);
+  padding: 2.6rem;
+  border: 0px;
+  margin: 0px;
+  color: white;
+  text-align: left;
+  display: block;
+  padding-left: 1rem;
+  }
+.search-box p{
+  font-size: 2em;
+  margin: 0%;
+}
+.search-bar{
+  width: 80%;
+  height: 3em;
+  border-radius: 5px;
+  
+}
+
+.tabel-header{
+  width: 30%;
+  font-size: 1.5em;  
+}
+
+.block-count p{
+  color: rgb(33, 216, 33);
+  font-weight: 500;
+  font-size: 1.2em;
+  background-color: rgba(255, 255, 255, 0.9);
+  margin: auto;
+  width: 20%;
+  text-align: center;
+  
+}
+
 </style>
+
+
